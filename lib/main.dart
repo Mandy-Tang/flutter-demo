43fa27cb62984853d27c2563e9e334025123610c
Tang Man <189658646@qq.com>
2021-05-25 12:26:32 +0800

    pushed to the saved suggestions page

--- lib/main.dart
+++ lib/main.dart
@@ -56,10 +56,27 @@ class _RandomWordsState extends State<RandomWords> {
     );
   }
 
+  void _pushSaved() {
+    Navigator.of(context)
+        .push(MaterialPageRoute<void>(builder: (BuildContext context) {
+      final tiles = _saved.map((WordPair pair) =>
+          ListTile(title: Text(pair.asPascalCase, style: _biggerFont)));
+      final divided =
+          ListTile.divideTiles(context: context, tiles: tiles).toList();
+
+      return Scaffold(
+          appBar: AppBar(title: Text('Saved Suggestions')),
+          body: ListView(children: divided));
+    }));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Startup Name Generator')),
+      appBar: AppBar(
+        title: Text('Startup Name Generator'),
+        actions: [IconButton(onPressed: _pushSaved, icon: Icon(Icons.list))],
+      ),
       body: _buildSuggestions(),
     );
   }
